ccf2041b298f06828a850450a1240d55b957dae4
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-12 22:44:34 +0630

    adding the code fo Testimonial.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Features from "./components/Features";
 import NewItems from "./components/NewItems";
 import FeaturesSecond from "./components/FeaturesSecond";
 import Products from "./components/Products";
+import Testimonial from "./components/Testimonial";
 
 const App = () => {
 	return (
@@ -15,6 +16,7 @@ const App = () => {
 			<NewItems />
 			<FeaturesSecond />
 			<Products />
+			<Testimonial />
 		</div>
 	);
 };
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,10 +1,31 @@
 import React from "react";
 
+//import components
+import TestimonialSlider from "../components/TestimonialSlider";
+
+//import data
+import { testimonial } from "../data";
+
 const Testimonial = () => {
+	//destructure testimonial
+	const { image, title } = testimonial;
 	return (
-		<div>
-			<div>Testimonial</div>
-		</div>
+		<section className="section">
+			<div className="container mx-auto">
+				<div className="flex flex-col lg:flex-row lg:gap-x-10 gap-y-2">
+					<div className="lg:max-w-[50%]">
+						{/* title */}
+						<h2 className="title mb-9">{title}</h2>
+						{/* slider */}
+						<TestimonialSlider />
+					</div>
+					{/* image */}
+					<div className="order-1">
+						<img src={image.type} alt="/" />
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
