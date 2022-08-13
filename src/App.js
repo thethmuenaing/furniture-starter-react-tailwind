6daf8cc2a0cde80a7ee30874ed992afe1d954af2
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-08-13 19:42:13 +0630

    adding the code of Header.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 //import components
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import NewItems from "./components/NewItems";
@@ -13,6 +14,7 @@ import Footer from "./components/Footer";
 const App = () => {
 	return (
 		<div className="w-full max-w-[1440px] mx-auto bg-white">
+			<Header />
 			<Hero />
 			<Features />
 			<NewItems />
